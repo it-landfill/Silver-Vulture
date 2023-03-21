4436d4082281beca012f7eac2e9c81e66a6422b0
Nemesis <lorenzo.balugani@gmail.com>
2023-03-21 17:50:38 +0100

    :sparkles: Data loader

---
+++ src/main/scala/Anime.scala
@@ -0,0 +1,5 @@
+import org.apache.spark.sql.DataFrame
+
+class Anime (val mal_id: Any, val genre: Any, val studio: Any, val ratings: Any = null) {
+
+}
---
+++ src/main/scala/Vectorizer.scala
@@ -0,0 +1,7 @@
+import org.apache.spark.sql.DataFrame
+
+object Vectorizer{
+  def vectorize(anime:DataFrame, ratings:DataFrame) = {
+    print("Now importing ")
+  }
+}

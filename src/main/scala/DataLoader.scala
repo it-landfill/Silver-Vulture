524d4e627bdedb00ff05807b204ca36b31c13792
Alessandro Benetton <aleben98@gmail.com>
2023-03-26 16:14:37 +0200

    [master] [SV-6] Completed data load and import from main

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -1,37 +1,17 @@
-import org.apache.spark.sql.{DataFrame, Encoders, Row, SparkSession}
-
-import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
-import scala.collection.mutable
+import org.apache.spark.sql.{DataFrame, SparkSession}
 // https://spark.apache.org/docs/latest/sql-data-sources-csv.html
 
-
-object DataLoader {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder()
-      .appName("Silver-Vulture")
-      .config("spark.master", "local")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("FATAL")
-    val anime = loadCSV(spark, path = "data/anime.csv", headers = true, filteredColumns = Array("MAL_ID", "Genres", "Studios"))
-    println("Anime data loading complete!")
-    val user_ratings = loadCSV(spark, path = "data/rating_complete.csv", headers = true, null)
-    println("User ratings loading complete!")
-    println("Now converting to internal representation...")
-    val animes = anime.collectAsList().par
-    val result = animes.map(anime => new Anime(anime.get(0), anime.get(1), anime.get(2), user_ratings.filter(user_ratings("anime_id")===anime.get(0)))).par
-
-    println("Done.")
-  }
-
-  def loadCSV(session: SparkSession, path: String, headers: Boolean, filteredColumns: Array[String]): DataFrame = {
-    val df = session
-      .read.option("header", "true")
-      .csv(path)
-    if (filteredColumns != null) {
-      df.select(filteredColumns.head, filteredColumns.tail: _*)
-    } else {
-      df
+class DataLoader(session: SparkSession) {
+    def loadCSV(
+        path: String,
+        headers: Boolean,
+        filteredColumns: Array[String]
+    ): DataFrame = {
+        val df = session.read.option("header", headers).csv(path)
+        if (filteredColumns != null) {
+            df.select(filteredColumns.head, filteredColumns.tail: _*)
+        } else {
+            df
+        }
     }
-  }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
+import org.apache.spark.sql.SparkSession
+
 object Main {
     def main(args: Array[String]) = {
 
+        // Generate spark session
+        val sparkSession = SparkSession
+            .builder()
+            .appName("Silver-Vulture")
+            .config("spark.master", "local")
+            .getOrCreate()
+
+        // Load DataLoader
+        val dataLoader = new DataLoader(sparkSession);
+        val rating_complete = dataLoader.loadCSV("data/rating_complete.csv", true, null);
+        rating_complete.show();
     }
 }
